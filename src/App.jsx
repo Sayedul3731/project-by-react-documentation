1e8a94c29112c181892d027fd87a32f808d2c901
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-09-08 19:22:01 -0700

    complete the porject

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import MyApp from './MyApp';
 import AboutPage from './About';
+import Profile from './Profile';
+import ShoppingList from './Shopping';
+import MyButton from './Mybutton';
 
 function App() {
   return (
     <>
       <MyApp></MyApp>
       <AboutPage></AboutPage>
+      <Profile></Profile>
+      <ShoppingList></ShoppingList>
+      <MyButton></MyButton>
     </>
   )
 }
